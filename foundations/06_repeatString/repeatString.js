ea12e0cdf22aa972a416d2a3b6692a37e2d7660e
Leo Funk <leo.funk1@hotmail.com>
2026-03-24 21:36:58 +0100

    Completed problem 6
    nice

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    // handles when num < 2
+    if(num < 2) {
+        switch (num) {
+            case 1:
+                return string;
+            case 0:
+                return ""
+            default:
+                return "ERROR"
+        }
+    }
 
+    let repeat = "";
+    for(let i = 0; i < num; i++) {
+        repeat = repeat.concat(string);
+    }
+    return repeat;
 };
 
 // Do not edit below this line
