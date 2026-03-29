8f9c3fa6bf1b9f15f49f93eed14ff91bedc34994
Leo Funk <leo.funk1@hotmail.com>
2026-03-29 20:40:08 +0200

    Completed problem #7 Finished the reverseString problem

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let array = text.split("");
+    let reversed = [];
+    for(let i = 0; i < array.length; i++) {
+        reversed[i] = array[array.length - 1 - i]
+    }
+    return reversed.join("")
 };
 
 // Do not edit below this line
