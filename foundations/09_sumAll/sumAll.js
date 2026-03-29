ba5961fe46d268157435e0ad12716b58aa74ae22
Leo Funk <leo.funk1@hotmail.com>
2026-03-29 21:22:41 +0200

    completed problem #9 Completed "sumAll"

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(!Number.isInteger(start) || !Number.isInteger(end)) {
+        return("ERROR")
+    }
+    if(start > end) {
+        let saved = start;
+        start = end;
+        end = saved;
+    }
+    if(start < 0) {
+        return("ERROR");
+    }
 
+    let acc = start + end;
+    for(let i = start + 1; i < end; i++) {
+        acc = acc + i;
+    }
+    return(acc);
 };
 
 // Do not edit below this line
