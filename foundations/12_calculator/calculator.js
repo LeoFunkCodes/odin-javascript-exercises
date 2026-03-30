a9f6cb4e45cd0c65fca04f9d75850b2d59b28e08
Leo Funk <leo.funk1@hotmail.com>
2026-03-30 07:23:00 +0200

    Completed problem #12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 0) {
+    return 0;
+  }
+	return arr.reduce((acc, current) => acc + current)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, current) => acc * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for(let i = 1; i <= a; i++) {
+    factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
