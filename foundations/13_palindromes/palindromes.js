3479dcf52426630bdbeaba5d0834372a05bc5210
Leo Funk <leo.funk1@hotmail.com>
2026-04-12 10:55:17 +0200

    Finished problem #13 and #14

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lettersAndNumbers = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const chars = string.toLowerCase().split("").filter(current => lettersAndNumbers.includes(current));
+    
+    const reversed = [...chars].reverse().join("");
+    string = chars.join("");
+
+    return reversed == string;
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if(index < 0) {
+        return("OOPS");
+    }
 
+    let numbers = [0, 1]
+    for(let i = 2; i < 100; i++) {
+        numbers.push(numbers[i - 1] + numbers[i - 2]);
+    }
+    return(numbers[index]);
 };
 
 // Do not edit below this line
