7cd6c6ed850c462448270fdeab7b28c3666ca13b
Leo Funk <leo.funk1@hotmail.com>
2026-04-12 11:23:32 +0200

    Finished problems 15 and 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    people.sort((a, b) => {
+
+        const year = new Date().getFullYear();
+        if(!a.yearOfDeath > 0 && !a.yearOfDeath < 99999) {
+            a.yearOfDeath = year;
+        }
+        if(!(b.yearOfDeath > 0) && !(b.yearOfDeath < 99999)) {
+            b.yearOfDeath = year;
+        }
+
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    })
+
+    return people[0];
 
 };
 
